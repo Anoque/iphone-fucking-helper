9eb2391847c60db1e4e26c4d3eb58402ce0f7029
Ayas <anoque@mail.ru>
2019-04-04 18:45:51 +0700

    Add example for getting data from backend

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {NetService, ResponseData} from './shared/net/net.service';
+
+export class Article {
+  id: number;
+  title: string;
+  date: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  articles: Article[];
+
+  constructor(private netService: NetService) {
+    this.articles = [];
+  }
+
+  ngOnInit(): void {
+    this.netService.getRequest('articles/').subscribe((res: any) => {
+      if (res) {
+        this.articles = res;
+      }
+    });
+  }
 }
