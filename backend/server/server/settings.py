b22dd0f2df46dab525870ca91d629689199579f4
Andrey <andrei.minaev.m@yandex.ru>
2019-04-05 21:49:37 +0700

    Password BD
    qwerty

--- backend/server/server/settings.py
+++ backend/server/server/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'iphone-app',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'qwerty',
         'HOST': 'localhost',
         'PORT': '3306',
     }
