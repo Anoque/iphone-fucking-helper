1f18db7cc1e5b9a55fe54d45c49230c13bf12c66
Ayas <anoque@mail.ru>
2019-04-10 00:52:15 +0700

    Update article module

--- frontend/src/app/articles/articles.module.ts
+++ frontend/src/app/articles/articles.module.ts
@@ -2,20 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleFormComponent } from './article-form/article-form.component';
 import { ArticlesListComponent } from './articles-list/articles-list.component';
-import {MaterialModule} from '../shared/material/material.module';
-import {RouterModule, Routes} from '@angular/router';
-import {ArticlesRoutingModule} from './articles-routing.module';
+import { MaterialModule } from '../shared/material/material.module';
+import { RouterModule } from '@angular/router';
+import { ArticlesRoutingModule } from './articles-routing.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_DATE_LOCALE } from '@angular/material';
 
 
 @NgModule({
   imports: [
     CommonModule,
+    BrowserAnimationsModule,
     ArticlesRoutingModule,
-    MaterialModule
+    MaterialModule,
+    ReactiveFormsModule
   ],
   exports: [
     RouterModule
   ],
-  declarations: [ArticleFormComponent, ArticlesListComponent]
+  declarations: [
+    ArticleFormComponent,
+    ArticlesListComponent
+  ],
+  providers: [
+    { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+  ]
 })
 export class ArticlesModule { }
