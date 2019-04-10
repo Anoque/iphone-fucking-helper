489689f966e12230cd41af101c851a11c607b00f
Ayas <anoque@mail.ru>
2019-04-10 10:24:33 +0700

    Remove service connection in main page

--- frontend/src/app/main-page/main-page.component.ts
+++ frontend/src/app/main-page/main-page.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {NetService} from '../shared/net/net.service';
-import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-main-page',
@@ -9,7 +7,7 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 export class MainPageComponent implements OnInit {
 
-  constructor(private netService: NetService, private router: Router) { }
+  constructor() { }
 
   ngOnInit(): void { }
 }
