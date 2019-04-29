33093c6ada618258750ebe2227f651b4a5fc9f6c
Ayas <anoque@mail.ru>
2019-04-29 14:52:34 +0700

    Change article info component

--- frontend/src/app/articles/article-info/article-info.component.ts
+++ frontend/src/app/articles/article-info/article-info.component.ts
@@ -1,12 +1,12 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
-import {NetService} from '../../shared/net/net.service';
-import { ArticleRelation } from '../../main-page/main-page.component';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { NetService } from '../../shared/net/net.service';
 import { Article } from '../articles-list/articles-list.component';
 
 export interface ArticlePage {
   article: Article;
-  relations: ArticleRelation[];
+  children: Article[];
+  parents: Article[];
 }
 
 @Component({
@@ -36,10 +36,13 @@ export class ArticleInfoComponent implements OnInit {
   }
 
   loadData(): void {
-    this.netService.getRequest('articles/get_article/' + this.id + '/', true).subscribe((res) => {
+    this.netService.getRequest('articles/get/' + this.id + '/', true).subscribe((res) => {
       this.data = res.data;
-      this.data.relations.forEach(_ => this.opened.push(false));
     });
+  }
+
+  toArticleInfoPage(i: number): void {
+    this.router.navigate(['articles/' + this.data.children[i].id]);
   }
 
 }
